fbf9cb3119b4d895fbd574a7763fbad4971c27ec
lshep <lori.shepherd@roswellpark.org>
2020-01-29 08:21:41 -0500

    User requested Deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,9 @@
 {
   library.dynam("AnalysisPageServer", pkg, lib)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.12")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
